65074bf8b3d6a595b83033591a654ac98378e3f7
Matan Crispel <matan.cris@gmail.com>
2021-01-12 16:45:31 +0200

    add start msg option

--- src/cmps/ContactPreview.jsx
+++ src/cmps/ContactPreview.jsx
@@ -6,7 +6,7 @@ export function ContactPreview({contact, onSendCheckMsg, onDeleteContact, onSend
             <h1>{contact.name} - {contact.num}</h1>
             <button onClick={() => onSendCheckMsg(contact)}>send check msg</button>
             <button onClick={() => onSendInvitation(contact.num)}>send invitation msg</button>
-            <button onClick={() => onSendStartMsg(contact.num)}>send invitation msg</button>
+            <button onClick={() => onSendStartMsg(contact.num)}>send start msg</button>
             <button onClick={() => onDeleteContact(contact.id)}>X</button>
         </li>
     )
