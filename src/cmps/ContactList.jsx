e5e374a8178f1299a583d06dc769b096c5365712
Matan Crispel <matan.cris@gmail.com>
2021-01-13 15:54:33 +0200

    add status colors

--- src/cmps/ContactList.jsx
+++ src/cmps/ContactList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ContactPreview } from './ContactPreview'
 
 // export function ContactList({ contacts, onSendCheckMsg, onDeleteContact, onSendInvitation , onSendStartMsg }) {
-export function ContactList({ contacts, onSendMsg }) {
+export function ContactList({ contacts, onSendMsg, onDeleteContact }) {
     console.log("🚀 ~ file: ContactList.jsx ~ line 4 ~ ContactList ~ contacts", contacts)
     return (
         <ul className="contact-list clean-list">
