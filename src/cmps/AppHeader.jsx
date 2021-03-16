d3f7838b47e054782c2360581afd71cfbfd40e43
Matan Crispel <matan.cris@gmail.com>
2021-03-16 11:13:52 +0200

    header fix

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function AppHeader() {
     return (
-        <section className="app-header">
+        <section className="app-header flex justify-center">
             <Link to='/'>home</Link>| 
             <Link to='/editMsg'>edit messeges</Link>
         </section>
