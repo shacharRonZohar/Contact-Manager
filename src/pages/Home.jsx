a1d6f4b95e2eb3129017cddc4137807965c2fd72
Matan Crispel <matan.cris@gmail.com>
2021-05-13 13:20:18 +0300

    responsive aside-bar + refactoring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react'
+import AsideBar from '../cmps/AsideBar'
 import { LeadFilter } from '../cmps/lead/LeadFilter'
 import { LeadList } from '../cmps/lead/LeadList'
 import LeadSavedLists from '../cmps/lead/LeadSavedList'
 import { leadService } from '../services/leadService'
-import { storageService } from '../services/storageService'
 
 export function Home() {
     const [leads, setLeads] = useState([])
     const [lead, setLead] = useState({ name: '', num: '', status: '', info: '' })
     const [zoomUrl, setZoomUrl] = useState('')
     const [filter, setFilter] = useState({ status: '' })
-    const [listToSave, setSavedList] = useState({ title: ''})
+    const [listToSave, setSavedList] = useState({ title: '' })
     const [savedLists, setSavedLists] = useState([])
 
     useEffect(() => {
@@ -55,11 +55,6 @@ export function Home() {
         loadLeads()
     }
 
-    function onClearStorage() {
-        storageService.saveToStorage('myLeads', null)
-        window.location.reload();
-    }
-
     const onSetZoomUrl = (ev) => {
         setZoomUrl(ev.target.value)
     }
@@ -88,7 +83,6 @@ export function Home() {
         // the only one that is required
         const { destination, source, draggableId } = res;
         if (!destination) return
-        console.log("🚀 ~ file: Home.jsx ~ line 91 ~ onDragEnd ~ destination", destination)
         const newIdx = destination.index
         const prevIdx = source.index
         leadService.updateLeadIdx(newIdx, prevIdx)
@@ -99,35 +93,29 @@ export function Home() {
         leadService.setLeadsList(currLeads)
         loadLeads()
     }
-    
+
 
     return (
         <main className="homepage">
-            <section className="main-section flex column space-around align-center">
-                <button className="clear-btn" onClick={() => onClearStorage()}>clear storage</button>
-                <form className="main-form flex column align-center" onSubmit={(ev) => onAddLead(ev)}>
-                    <h3>Add Lead</h3>
-                    <input type="text" onChange={onInputChange} name="num" value={lead.num} placeholder="num" />
-                    <input type="text" onChange={onInputChange} name="name" value={lead.name} placeholder="name" />
-                    <button>add</button>
-                </form>
-                <div className="zoom-wrapper">
-                    <h3>Insert Url</h3>
-                    <input type="text" placeholder="ZOOM url" onChange={onSetZoomUrl} />
-                </div>
-                <form className="save-list-form flex column align-center" onSubmit={onSaveList}>
-                    <h3>Save List</h3>
-                    <input type="text" name="list-title" placeholder="insert title" onChange={onInputChange} value={listToSave.title} required />
-                    <button className="save-list-btn">save this list</button>
-                </form>
-                <button onClick={onSendAll}>send all</button>
-            </section>
+            <AsideBar
+                lead={lead}
+                listToSave={listToSave}
+                onAddLead={onAddLead}
+                onInputChange={onInputChange}
+                onSetZoomUrl={onSetZoomUrl}
+                onSaveList={onSaveList}
+                onSendAll={onSendAll} />
             <section className="main-list">
                 <div className="main-list-header">
                     <h2>on the list now: {leads.length}</h2>
                     <LeadFilter onSetFilter={onSetFilter} />
                 </div>
-                <LeadList leads={leads} onDragEnd={onDragEnd} onSendMsg={onSendMsg} onDeleteLead={onDeleteLead} onAddInfo={onAddInfo} />
+                <LeadList
+                    leads={leads}
+                    onDragEnd={onDragEnd}
+                    onSendMsg={onSendMsg}
+                    onDeleteLead={onDeleteLead}
+                    onAddInfo={onAddInfo} />
             </section>
             <LeadSavedLists savedLists={savedLists} setCurrList={setCurrList} />
         </main>
